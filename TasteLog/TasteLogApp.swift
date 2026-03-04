40e1062cb1fcc70e7583486eb2f77b58efeecedd
haruka <fukushima.h@keio.jp>
2026-03-04 16:35:25 +0900

    Initial Commit

---
+++ TasteLog/TasteLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteLogApp.swift
+//  TasteLog
+//
+//  Created by Fukushima Haruka on 2026/03/04.
+//
+
+import SwiftUI
+
+@main
+struct TasteLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
